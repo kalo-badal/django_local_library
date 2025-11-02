b3f8872583a34a6ee9246b0ab3bcb4eff955f899
kalo-badal <paraj4sx@dukes.jmu.edu>
2025-11-02 13:02:12 -0500

    Lab_10

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -15,3 +15,11 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
+
+urlpatterns += [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
+
+urlpatterns += [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
